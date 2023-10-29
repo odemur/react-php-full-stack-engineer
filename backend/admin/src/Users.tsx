c0cb183c6bab40dd4d01a823bfdb688ff5de3a42
Odemur Marangoni <dev@odemur.com.br>
2023-10-29 12:52:23 -0300

    Added live search

--- backend/admin/src/Users.tsx
+++ backend/admin/src/Users.tsx
@@ -12,6 +12,8 @@ interface User {
 
 function Users() {
   const [users, setUsers] = useState<User[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
 
   function handleEmailClick(user: User) {
     sendEmail(user.id, user.name, user.email);
@@ -20,23 +22,23 @@ function Users() {
   // Enviar email para o back-end
   function sendEmail(id: number, name: string, email: string) {
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ id, name, email })
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id, name, email }),
     };
 
     fetch(process.env.REACT_APP_API_URL + "/api/sendmail", requestOptions)
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         console.log(data);
       })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
       });
   }
 
@@ -50,6 +52,7 @@ function Users() {
       if (response.ok) {
         const userData = await response.json();
         setUsers(userData);
+        setFilteredUsers(userData);
       }
     } catch (error) {
       console.error("Erro ao obter usuários cadastrados:", error);
@@ -63,12 +66,25 @@ function Users() {
   return (
     <div>
       <h2>Users</h2>
+      <input
+        type="text"
+        placeholder="Pesquisar..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
       <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} - {user.email} <button onClick={() => handleEmailClick(user)}>Enviar</button>
-          </li>
-        ))}
+        {filteredUsers
+          .filter(
+            (user) =>
+              user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+              user.email.toLowerCase().includes(searchQuery.toLowerCase())
+          )
+          .map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.email}{" "}
+              <button onClick={() => handleEmailClick(user)}>Enviar</button>
+            </li>
+          ))}
       </ul>
       {/* add Bootstrap  */}
     </div>

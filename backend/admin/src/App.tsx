da3be659cf1f972ca9741ecbb29c6261bb152997
Odemur Marangoni <dev@odemur.com.br>
2023-10-29 09:03:58 -0300

    Added App.tsx and App.test.js

---
+++ backend/admin/src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import UserForm from "./UserForm";
+import Login from "./Login";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<UserForm />} />
+        </Routes>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/cadastro">Cadastro</Link>
+            </li>
+          </ul>
+        </nav>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
